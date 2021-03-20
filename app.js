8a12d076b98d969e03c5c6f2edd184d41a02049d
jerkan685 <jerkanaf@gmail.com>
2021-03-19 23:23:34 -0300

    upload

---
+++ app.js
@@ -0,0 +1,28 @@
+var edadJerkan, edadKanjer, diferenciaEdad;
+var sumaEdades, yearJerkan, yearKanjer, yearActual, anoJerkan, anoKanjer;
+
+edadJerkan = (23);
+edadKanjer = (16);
+yearActual = (2020);
+yearJerkan = (1996);
+yearKanjer = (2002);
+diferenciaEdad = edadJerkan - edadKanjer;
+anoJerkan = yearActual - yearJerkan;
+anoKanjer = yearActual - yearKanjer
+
+var mercado = new Array('Cafe','Tomate','Naranja');
+
+
+mercado.push = ('jose');
+mercado.unshift = ('inicio');
+
+var boton = document.getElementById ('boton');
+boton.addEventListener('click', mensaje)
+function mensaje() {
+    alert('holaaa perro')
+}
+
+var botona = document.getElementsByTagName('button')
+
+console.log('botona')
+
